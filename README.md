5ad85de4a11b8cfcd5b4ead9b98b7d09f1d11ecf
Syed Salman <syedsalman1006@gmail.com>
2023-02-16 19:53:38 +0530

    Added a Success route to redirect on successfull payment confirmation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# game-store-strapi
+# Online Video Games Store 
+This is a front end application integrated with CMS(content management sysytem) using strapi;
+
+Online payment gateway is also added using Stripe;
+test card:-
+card number: 4242 4242 4242 4242
+expire date: any future date
+cvv: any three digit number
+
+use this test card to do the payments
--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from "./components/Cart/Cart";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar"
+import Success from "./components/Success/Success";
 import CartState from "./context/Cart/CartState";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
                     <Routes>
                         <Route path='/' element={<Home />} />
                         <Route path="/cart" element={<Cart />} />
+                        <Route path="/success" element={<Success/>} />
                     </Routes>
                 </CartState>
             </BrowserRouter>
